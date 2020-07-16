0349cc62285758416794103d2fdc3c61effac143
Sergey Klimov <desire0417@gmail.com>
2020-07-16 10:58:24 +0300

    исправляет ошибки в js

--- source/js/main.js
+++ source/js/main.js
@@ -6,20 +6,24 @@
   var navList = mainNav.querySelector('.nav__list');
   var navLinks = navList.querySelectorAll('a');
 
+  mainNav.classList.remove('nav--nojs');
+
   var replaceClasses = function (element, addClass, removeClass) {
     element.classList.add(addClass);
     element.classList.remove(removeClass);
   };
 
-  navLinks.forEach(function (it) {
-    it.addEventListener('click', function () {
-      if (mainNav.classList.contains('nav--opened')) {
-        replaceClasses(mainNav, 'nav--closed', 'nav--opened');
-      }
-    });
-  });
+  var onClickLink = function () {
+    if (mainNav.classList.contains('nav--opened')) {
+      replaceClasses(mainNav, 'nav--closed', 'nav--opened');
+    }
+  }
 
-  mainNav.classList.remove('nav--nojs');
+  for (var i = 0; i < navLinks.length; i++) {
+    navLinks[i].addEventListener('click', onClickLink);
+  }
+
+
 
   headerToggle.addEventListener('click', function () {
     if (mainNav.classList.contains('nav--closed')) {
